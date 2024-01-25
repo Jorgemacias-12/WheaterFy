623ce493ed530e31c39de14d2ad1e5aba8c3965b
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-01-25 02:55:26 -0600

    Initial commit: xamarin project generated

---
+++ WeatherFy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WeatherFy.ViewModels;
+using Xamarin.Forms;
+
+namespace WeatherFy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
